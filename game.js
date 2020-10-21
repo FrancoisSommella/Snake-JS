f3f55a5e50f4083c80fedef87121b9c7cf4a85bf
Francois Sommella <francois.sommella@gmail.com>
2020-10-21 23:15:32 +0200

    snake speed

--- game.js
+++ game.js
@@ -3,4 +3,9 @@ const SNAKE_SPEED = 2
 
 function main(currentTime) {
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  window.requestAnimationFrame(main)
+  lastRenderTime = currentTime
+  console.log(secondsSinceLastRender)
 }
+
+window.requestAnimationFrame(main)
